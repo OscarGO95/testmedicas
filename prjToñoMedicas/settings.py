b537ddcc34d48e257796823bbcd962960f0aaef6
oscar <oscariva1995@gmail.com>
2017-09-21 17:45:44 -0500

    kjhgkhygkg

--- prjToñoMedicas/settings.py
+++ prjToñoMedicas/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT =os.path.join(BASE_DIR,"static")
+STATIC_ROOT =os.path.join(BASE_DIR,"staticfiles")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
